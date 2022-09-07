971e1c9c8d331dca80771a4efa8586047f7a2089
JoppeJr <105011426+JoppeJr@users.noreply.github.com>
2022-09-07 09:36:28 +0200

    json updated

--- JsFolder/common.js
+++ JsFolder/common.js
@@ -9,15 +9,15 @@ function logOut() {
 
 }
 
-function checkJson() {
-    fetch('https://ord.uib.no/bm/fil/lemma.json')
+async function checkJson() {
+    let response = await fetch('https://ord.uib.no/bm/fil/lemma.json')
         .then((response) => response.json())
         .then((json) => model.randomWordArray.push(json));
 
     console.log(model.randomWordArray)
 
 
-
+    getRightFromJson();
     updateView();
 }
 
@@ -40,13 +40,21 @@ function getRightFromJson() {
     //   for(i = 0; i < model.randomWordArray[28].length; i++){
     //  console.log(model.randomWordArray[i][0])
     //  }
-
-
-
-
     // here we get the array with arrays of 3. then we need 2 make it array og arrays with 1
+}
+// gettekst()
+// async function gettekst(){
+//     let response = await fetch("Ordliste5Bokstaver.txt");
+//     let text = await response.text()
+//     let fjernNewlineOgDiv = await text.replace(/\r\n/g, " ")
+//     let stringArray = await fjernNewlineOgDiv.toString().split(" "); 
+//     console.log(stringArray)
+// }
 
-} // json fil for ord liste med lister i lister i lister
+
+
+
+// json fil for ord liste med lister i lister i lister
 
 // function getRightFromJson() {
 //     console.log(model.randomWordArray)
